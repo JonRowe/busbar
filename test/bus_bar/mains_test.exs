672baec7c60192748f52b674410d26fcb896d832
Jon Rowe <hello@jonrowe.co.uk>
2016-09-23 18:45:00 +1000

    add extra listener tests

--- test/bus_bar/mains_test.exs
+++ test/bus_bar/mains_test.exs
@@ -8,15 +8,31 @@ defmodule BusBar.MainsTest do
     use GenEvent
     require Logger
 
-    def handle_event({:attach_test, _ }, parent) do
-      Logger.info "Attach test success"
+    def handle_event({:attach_test, data }, parent) do
+      Logger.info "Attach test success #{data}"
       { :ok, parent }
     end
 
-    def handle_event({:notify_test, _ }, parent) do
-      Logger.info "Notify test success"
+    def handle_event({:notify_test, data }, parent) do
+      Logger.info "Notify test success #{data}"
       { :ok, parent }
     end
+  end
+
+  defmodule OtherTestHandler do
+    use GenEvent
+    require Logger
+
+    def handle_event({:attach_test, data }, parent) do
+      Logger.info "Other attach test success #{data}"
+      { :ok, parent }
+    end
+
+    def handle_event({:notify_test, data }, parent) do
+      Logger.info "Other notify test success #{data}"
+      { :ok, parent }
+    end
+
   end
 
   test "#start_link stores a gen event pid" do
@@ -28,19 +44,43 @@ defmodule BusBar.MainsTest do
     log = capture_log(fn ->
       pid = Agent.get(BusBar.Mains, fn (pid) -> pid end)
       BusBar.Mains.attach TestHandler
-      GenEvent.notify pid, {:attach_test, {}}
+      GenEvent.notify pid, {:attach_test, '1'}
       GenEvent.sync_notify pid, :attach_test
     end)
-    assert log =~ ~r/Attach test success/
+    assert log =~ ~r/Attach test success 1/
+  end
+
+  test "#attach will allow multiple subscriptions" do
+    log = capture_log(fn ->
+      pid = Agent.get(BusBar.Mains, fn (pid) -> pid end)
+      BusBar.Mains.attach TestHandler
+      BusBar.Mains.attach OtherTestHandler
+      GenEvent.notify pid, {:attach_test, '2'}
+      GenEvent.sync_notify pid, :attach_test
+    end)
+    assert log =~ ~r/Attach test success 2/
+    assert log =~ ~r/Other attach test success 2/
   end
 
   test "notify will transmit events via genevent" do
     log = capture_log(fn ->
       pid = Agent.get(BusBar.Mains, fn (pid) -> pid end)
       GenEvent.add_handler pid, TestHandler, []
-      BusBar.Mains.notify :notify_test, :data
+      BusBar.Mains.notify :notify_test, 1
       GenEvent.sync_notify pid, :notify_test
     end)
     assert log =~ ~r/Notify test success/
+  end
+
+  test "notify will transmit events via genevent to multiple listeners" do
+    log = capture_log(fn ->
+      pid = Agent.get(BusBar.Mains, fn (pid) -> pid end)
+      GenEvent.add_handler pid, TestHandler, []
+      GenEvent.add_handler pid, OtherTestHandler, []
+      BusBar.Mains.notify :notify_test, 2
+      GenEvent.sync_notify pid, :notify_test
+    end)
+    assert log =~ ~r/Notify test success 2/
+    assert log =~ ~r/Other notify test success 2/
   end
 end
