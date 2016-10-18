944c3960fa0488200aa52c55f2ebd9d6335c19b4
Jon Rowe <hello@jonrowe.co.uk>
2016-10-18 12:15:32 +1100

    add debug level log information

--- lib/bus_bar/mains.ex
+++ lib/bus_bar/mains.ex
@@ -1,23 +1,28 @@
 defmodule BusBar.Mains do
   @moduledoc false
+  require Logger
 
   def start_link do
     { :ok, pid } = GenEvent.start_link([])
+    Logger.debug "Started BusBar GenEvent at #{inspect pid}"
     Agent.start(fn -> pid end, name: __MODULE__)
     { :ok, pid }
   end
 
   def notify(event, data \\ nil) do
+    Logger.debug "BusBar NOTIFY #{event}"
     bus_process
     |> GenEvent.notify({event, data})
   end
 
   def attach(listener, args \\ []) do
+    Logger.debug "BusBar ATTACH #{listener}"
     bus_process
     |> GenEvent.add_handler(listener, args)
   end
 
   def detach(listener, args \\ []) do
+    Logger.debug "BusBar DETACH #{listener}"
     bus_process
     |> GenEvent.remove_handler(listener, args)
   end
@@ -27,6 +32,7 @@ defmodule BusBar.Mains do
   end
 
   def sync(event) do
+    Logger.debug "BusBar SYNC #{event}"
     bus_process |> GenEvent.sync_notify(event)
   end
 
