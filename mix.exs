c61d615711d586722051ac1b91984740c1b331ce
Jon Rowe <hello@jonrowe.co.uk>
2017-02-09 20:36:43 +1100

    expand method invocations to remove warning

--- mix.exs
+++ mix.exs
@@ -8,9 +8,9 @@ defmodule BusBar.Mixfile do
       elixir:          "~> 1.2",
       build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps:            deps,
-      description:     description,
-      package:         package,
+      deps:            deps(),
+      description:     description(),
+      package:         package(),
     ]
   end
 
