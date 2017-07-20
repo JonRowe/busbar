56c8b1a931573dde533bcb4ca665a3d66d2a795f
Jon Rowe <hello@jonrowe.co.uk>
2017-07-20 14:15:20 +1000

    Ensure we can block a busbar

--- test/bus_bar_test.exs
+++ test/bus_bar_test.exs
@@ -12,6 +12,17 @@ defmodule BusBarTest do
       send(pid, :notify_test)
       {:ok, state}
     end
+
+    def handle_event({:long_notify_test, pid}, state) do
+      Process.sleep 200
+      send(pid, :long_notify_test)
+      {:ok, state}
+    end
+
+    def handle_event({:sync_notify_test, pid}, state) do
+      send(pid, :sync_notify_test)
+      {:ok, state}
+    end
   end
 
   setup do
@@ -45,6 +56,19 @@ defmodule BusBarTest do
   test "#sync_notify doesn't block when no matching event" do
     BusBar.sync_notify :not_notify_test, self()
     refute_received :not_notify_test
+  end
+
+  test "#sync_notify blocks until sync finished" do
+    BusBar.notify :long_notify_test, self()
+    BusBar.sync_notify :sync_notify_test, self()
+
+    first = receive do
+      message -> message
+    end
+    second = receive do
+      message -> message
+    end
+    assert [first, second] == [:long_notify_test, :sync_notify_test]
   end
 
   test "#listeners" do
