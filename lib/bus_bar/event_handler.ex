bb4bcc8a062113716384b2613b9c96110ea36b1f
Jon Rowe <hello@jonrowe.co.uk>
2017-09-27 21:54:15 +1000

    Always log stacktraces

--- lib/bus_bar/event_handler.ex
+++ lib/bus_bar/event_handler.ex
@@ -44,8 +44,8 @@ defmodule BusBar.EventHandler do
   end
 
   defp handle_error(handler, e) do
-    Logger.error "Error occured in #{handler}, #{inspect e}"
-    Logger.debug inspect System.stacktrace()
+    Logger.error "Error occured in #{handler}, #{inspect e} \n" <>
+                 inspect System.stacktrace()
     {:error, e}
   end
 end
